c880c45649660972a26973e415ea1fac2ab11eb4
BomScoob12 <sorawitkraukham@gmail.com>
2023-04-08 15:08:21 +0700

    change design

--- src/main/java/accounts/Customer.java
+++ src/main/java/accounts/Customer.java
@@ -4,7 +4,7 @@ import java.util.Date;
 import java.util.List;
 import java.util.Objects;
 
-public abstract class Customer {
+public class Customer {
     private List<Account> accounts;
     private final String idCard;
     private String firstname;
